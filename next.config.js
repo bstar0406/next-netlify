4016b851ab942684c0bb414d3700a14b81a647d7
eros0307 <eros0307@outlook.com>
2021-06-19 04:14:31 +0300

    4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['60ccecf9ee99be0007efc4e3--boring-austin-d1ef6e.netlify.app'],
+    loader: 'imgix',
+    path: 'https://test-next.netlify.app/',
   },
 }
